48496cfd96057d0671954b9c0cc62869aba40e5d
Chun Lung Chan <junochan@astro01.cluster>
2026-08-27 09:25:42 +0200

    Fix lensed importance sampling with Dingo 0.9.8
    Rebuild LensedWaveformGenerator using the existing domain and checkpoint waveform settings instead of changing its class in place. Update the dingo-gw dependency to 0.9.8 and ignore generated build artifacts.

--- dingo_lensing/likelihood.py
+++ dingo_lensing/likelihood.py
@@ -3,5 +3,10 @@ from dingo.gw.likelihood import StationaryGaussianGWLikelihood
 from .waveform_generator import LensedWaveformGenerator
 
 class StationaryGaussianLensedGWLikelihood(StationaryGaussianGWLikelihood):
-    def reset_waveform_generator(self):
-        self.waveform_generator.__class__ = LensedWaveformGenerator
+    def reset_waveform_generator(self, waveform_settings):
+        """Rebuild the waveform generator with initialized lensing state."""
+        waveform_domain = self.waveform_generator.full_domain
+        self.waveform_generator = LensedWaveformGenerator(
+            domain=waveform_domain,
+            **waveform_settings,
+        )
--- dingo_lensing/result.py
+++ dingo_lensing/result.py
@@ -9,4 +9,7 @@ class LensedGWResult(DingoGWResult):
     def _build_likelihood(self, **kwargs):
         super()._build_likelihood(**kwargs)
         self.likelihood.__class__ = StationaryGaussianLensedGWLikelihood
-        self.likelihood.reset_waveform_generator()
+        waveform_settings = self.base_metadata["dataset_settings"][
+            "waveform_generator"
+        ]
+        self.likelihood.reset_waveform_generator(waveform_settings)
