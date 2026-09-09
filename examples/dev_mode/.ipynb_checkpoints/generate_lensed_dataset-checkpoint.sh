3e0e67784cf668b5e9df5e60bc9e01d8be8fbe8a
Chun-Lung Chan <chun-lung.chan@ldas-grid.ligo.caltech.edu>
2026-09-09 05:33:12 -0700

    Ignore Jupyter checkpoint files

--- examples/dev_mode/.ipynb_checkpoints/generate_lensed_dataset-checkpoint.sh
+++
@@ -1,6 +0,0 @@
-#export OPENBLAS_NUM_THREADS=1
-# create training_data
-#python generate_dataset.py --settings_file waveform_dataset_settings.yaml --out_file training_data/waveform_datasethdf5
-dingo_lensing_generate_dataset --settings_file waveform_dataset_settings.yaml --out_file training_data/waveform_dataset.hdf5
-
-
